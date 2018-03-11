c7e1f17c2711f8df3b64661ba36f450b6ac868d6
Boyman10 <contact@yasi.fr>
2018-03-11 20:55:32 +0100

    Adding action to handle the user management page

---
+++ struts-webapp/src/main/resources/org/struts/webapp/action/ManageUserAction.java
@@ -0,0 +1,23 @@
+package org.struts.webapp.action;
+
+/**
+ * Class mapping the manager user action
+ * @author John
+ * @version 1
+ */
+public class ManageUserAction extends ActionSupport {
+
+	// The actionsupport already bring interesting stuff
+	private Integer id;
+	
+	private List<User> listUsers;
+	private User user;
+	
+	public Integer getId() {
+		
+		return id;
+	}
+
+
+}
+
