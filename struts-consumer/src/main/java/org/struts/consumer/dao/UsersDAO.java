33f4f2b02f07166bc7e67342cc308e203d49e71b
Boyman10 <contact@yasi.fr>
2018-03-14 21:46:21 +0100

    User DAO is within consumer module

--- struts-consumer/src/main/java/org/struts/consumer/dao/UsersDAO.java
+++ struts-consumer/src/main/java/org/struts/consumer/dao/UsersDAO.java
@@ -6,11 +6,16 @@ import org.struts.model.User;
 
 /**
  * Our users DAO
+ * 
  * @author John
  *
  */
 public class UsersDAO {
 
+	/**
+	 * Method to get the list of users.
+	 * @return
+	 */
 	public List<User> getUsers() {
 		
 		return null;
