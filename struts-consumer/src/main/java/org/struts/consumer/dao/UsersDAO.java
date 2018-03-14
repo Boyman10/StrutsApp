2f41660613067d6ef8a7899f30223b06db00dabf
Boyman10 <contact@yasi.fr>
2018-03-14 21:45:28 +0100

    Adding User DAO within model module

---
+++ struts-consumer/src/main/java/org/struts/consumer/dao/UsersDAO.java
@@ -0,0 +1,18 @@
+package org.struts.consumer.dao;
+
+import java.util.List;
+
+import org.struts.model.User;
+
+/**
+ * Our users DAO
+ * @author John
+ *
+ */
+public class UsersDAO {
+
+	public List<User> getUsers() {
+		
+		return null;
+	}
+}
--- struts-webapp/src/org/struts/webapp/action/ManageUserAction.java
+++ struts-webapp/src/org/struts/webapp/action/ManageUserAction.java
@@ -16,6 +16,8 @@ public class ManageUserAction extends ActionSupport {
 	private static final long serialVersionUID = 7246774814524999309L;
 
 	// The actionsupport already bring interesting stuff
+	
+	// User id
 	private Integer id;
 	
 	private List<User> listUsers;
@@ -28,11 +30,22 @@ public class ManageUserAction extends ActionSupport {
 
 	/**
 	 * Method which returns users
+	 * replaces default execute method
 	 * @return success
 	 */
 	public String doList() {
 		
-		// Listing users here
+		// Retrieve the list of users using the manager factory having the userManager returning the users listing
+		//listUsers = WebappHelper.getManagerFactory().getUserManager().getListUsers();
+		return ActionSupport.SUCCESS;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String doDetail() {
+		
 		
 		return ActionSupport.SUCCESS;
 	}
