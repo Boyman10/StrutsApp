0ca4a17651d85ced580332a9df94b74d5752f26f
Boyman10 <contact@yasi.fr>
2018-03-18 17:57:06 +0100

    Fixing problem with spring-tx jar by removing from libraries

--- struts-consumer/src/main/java/org/struts/consumer/dao/UsersDAO.java
+++ struts-consumer/src/main/java/org/struts/consumer/dao/UsersDAO.java
@@ -1,14 +1,20 @@
 package org.struts.consumer.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.struts.model.User;
 
 /**
- * Our users DAO
- * using POstgresql
+ * Our users DAO using POstgresql
+ * 
  * @author John
  *
  */
@@ -16,27 +22,45 @@ import org.struts.model.User;
 public class UsersDAO {
 
 	private NamedParameterJdbcTemplate jdbc;
-	
+
 	/**
 	 * Empty constructor
 	 */
 	public UsersDAO() {
-		
+
 		System.out.println("Good on you users DAO is loaded fine !!");
 	}
-	
+
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+
 	/**
 	 * Method to get the list of users.
 	 * @return
 	 */
 	public List<User> getUsers() {
 		
-		return null;
+		return jdbc.query("SELECT * FROM user", new RowMapper<User>() {
+
+			@Override
+			public User mapRow(ResultSet rs, int nRows) throws SQLException {
+				
+				User user = new User();
+				user.setUsername(rs.getString("username"));
+				
+				return user;
+			}
+			
+			
+		}
+		
+				);
+				
 	}
-	
-	
+
 	public void createUser(User user) {
-		
-		
+
 	}
 }
