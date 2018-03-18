eddca5a1b872f54708523c0ae49c1627d3a14d8a
Boyman10 <contact@yasi.fr>
2018-03-18 17:05:07 +0100

    Adding spring-jdbc dependency to consumer module

--- struts-consumer/src/main/java/org/struts/consumer/dao/UsersDAO.java
+++ struts-consumer/src/main/java/org/struts/consumer/dao/UsersDAO.java
@@ -2,6 +2,7 @@ package org.struts.consumer.dao;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.struts.model.User;
 
